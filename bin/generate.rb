b39a1101a108efd417b99df251ed74b5dedf22e1
Mui Kai En <muikaien1@gmail.com>
2021-04-15 14:39:47 +0800

    Move main.rb to a bin folder

--- bin/generate.rb
+++ bin/generate.rb
@@ -1,5 +1,14 @@
+#!/usr/bin/env ruby
+
 require 'rmagick'
 require 'generator'
+
+if ARGV.first.nil?
+  puts <<-HELP
+    Usage: generate.rb <image-path>
+  HELP
+  exit 1
+end
 
 template = Magick::Image.read("template.bmp").first
 
